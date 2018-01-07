29e35990b858248240cf85c55df7ea41fe76a37a
“Kurtis-Robertsgit config --global user.email “kurtimus23@gmail.com <kurtimus23@gmail.com>
2018-01-06 18:15:29 -0700

    come on heroku

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Set Handlebars.
 var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine('handlebars', exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 // Import routes and give the server access to them.
